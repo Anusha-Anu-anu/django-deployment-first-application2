cc11e1b717bbdc745d787d37af6309f4191d8f1c
Anusha-Anu-anu <138768218+Anusha-Anu-anu@users.noreply.github.com>
2023-07-11 22:18:03 +0530

    Update settings.py

--- FirstProject/FirstProject/settings.py
+++ FirstProject/FirstProject/settings.py
@@ -58,7 +58,7 @@ ROOT_URLCONF = 'FirstProject.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR/'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -78,9 +78,17 @@ WSGI_APPLICATION = 'FirstProject.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
+    #'default': {
+     #   'ENGINE': 'django.db.backends.sqlite3',
+      #  'NAME': BASE_DIR / 'db.sqlite3',
+    #}
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'HOST': 'Anusha1224.mysql.pythonanywhere-services.com'
+        'PORT': '',		#3306 or 3308
+        'NAME': 'Anusha1224$moviedb',	#make-sure this DB is in MySQL8-DB
+        'USER': 'Anusha1224',
+        'PASSWORD': 'root@123',
     }
 }
 
